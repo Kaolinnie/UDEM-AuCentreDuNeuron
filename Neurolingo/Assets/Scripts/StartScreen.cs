868bb10a13e78774ff1f4248eaba0e9fb942f3e2
Kaolin <kaolin.stacey@gmail.com>
2024-03-23 12:55:29 -0400

    Created the on click functions

--- Neurolingo/Assets/Scripts/StartScreen.cs
+++ Neurolingo/Assets/Scripts/StartScreen.cs
@@ -1,9 +1,12 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class StartScreen : MonoBehaviour
 {
+    
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -17,8 +20,18 @@ public class StartScreen : MonoBehaviour
     }
     
     // Clicking the start button
-    void ClickStart() 
-    {
-        
+    public void ClickStart() {
+        Debug.Log("You clicked start game");
     }
+
+    public void ClickControls() {
+        Debug.Log("You clicked controls");
+
+    }
+
+    public void ClickLanguage() {
+        Debug.Log("You clicked the language button");
+
+    }
+
 }
